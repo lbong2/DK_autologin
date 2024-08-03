8a56030492be47c45d37abfd99fb43c4af2aba06
longbee2 <longbee46@gmail.com>
2024-08-03 16:44:38 +0900

    first Commit

---
+++ auto_login/auth.py
@@ -0,0 +1,33 @@
+import imaplib
+import email
+from email.header import decode_header, make_header
+
+# 사용 전 구글 앱 비밀번호 설정해야 함.
+
+def getAuthNumber(mail, appPw, from_email):
+    server = imaplib.IMAP4_SSL("imap.gmail.com")  
+    server.login(mail, appPw)
+    server.select("inbox")
+
+    status, messages = server.search(None, f'(FROM "{from_email}")')
+
+    mail_ids = messages[0].split()  
+
+    if not mail_ids:  
+        print('error')
+
+    latest_email_id = mail_ids[-1]
+
+    status, msg_data = server.fetch(latest_email_id, "(RFC822)")
+
+    message = email.message_from_bytes(msg_data[0][1])
+    fr = make_header(decode_header(message.get('From')))
+    subject = make_header(decode_header(message.get('Subject')))
+
+
+    print(f"제목:{subject}")
+
+
+    server.close()
+    server.logout()
+    return str(subject).split('[')[1].split(']')[0]
---
+++ auto_login/main.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import time 
+import pyautogui
+import pyperclip
+
+import auth 
+
+
+iris_driver = webdriver.Chrome()
+iris_driver.get("https://iris.dongkuk.com/")
+
+id = iris_driver.find_element(By.CSS_SELECTOR, "#idtemp")
+id.click()
+pyperclip.copy("kyungbong.lee")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(2)
+
+pw = iris_driver.find_element(By.CSS_SELECTOR, "#passwordtemp")
+pw.click()
+pyperclip.copy("Dlrudqhd00@")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(2)
+
+login_bt = iris_driver.find_element(By.CSS_SELECTOR, "#frm > section > section > article.loginBox > div > div:nth-child(4) > a")
+login_bt.click()
+time.sleep(10)
+
+
+# 2차인증 
+auth_bt = iris_driver.find_element(By.CSS_SELECTOR, "#privateEmailButton")
+auth_bt.click()
+time.sleep(5)
+
+# 이메일 내용 크롤링
+auth_num = auth.getAuthNumber("rudqhd05193@gmail.com", "qncl mviv vieh ionv", "no_reply@worksmobile.com")
+# 인증번호 입력 
+pw2 = iris_driver.find_element(By.CSS_SELECTOR, "#number1")
+pw2.click()
+pyperclip.copy(auth_num)
+pyautogui.hotkey("ctrl", "v")
+time.sleep(2)
