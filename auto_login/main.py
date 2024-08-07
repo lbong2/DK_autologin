5b7152ed87b4343784d5e8187bc4e36ff0b4bd9c
KyungBong Lee <kyungbong.lee@dongkuk.com>
2024-08-07 17:09:02 +0900

    feat: time.sleep -> implicity_wait 변경

--- auto_login/main.py
+++ auto_login/main.py
@@ -8,40 +8,48 @@ import pyautogui
 import pyperclip
 
 import auth 
+
 # 브라우저 꺼짐 방지 옵션
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
 iris_driver = webdriver.Chrome(options=chrome_options)
 iris_driver.get("https://iris.dongkuk.com/")
+iris_driver.implicitly_wait(10)
 
 id = iris_driver.find_element(By.CSS_SELECTOR, "#idtemp")
 id.click()
 pyperclip.copy("kyungbong.lee")
 pyautogui.hotkey("ctrl", "v")
-time.sleep(2)
+iris_driver.implicitly_wait(10)
 
 pw = iris_driver.find_element(By.CSS_SELECTOR, "#passwordtemp")
 pw.click()
 pyperclip.copy("Dlrudqhd00@")
 pyautogui.hotkey("ctrl", "v")
-time.sleep(2)
+iris_driver.implicitly_wait(10)
 
 login_bt = iris_driver.find_element(By.CSS_SELECTOR, "#frm > section > section > article.loginBox > div > div:nth-child(4) > a")
 login_bt.click()
-time.sleep(10)
+iris_driver.implicitly_wait(10)
 
 
 # 2차인증 
 auth_bt = iris_driver.find_element(By.CSS_SELECTOR, "#privateEmailButton")
 auth_bt.click()
-time.sleep(5)
 
+time.sleep(5)
 # 이메일 내용 크롤링
 auth_num = auth.getAuthNumber("rudqhd05193@gmail.com", "qncl mviv vieh ionv", "no_reply@worksmobile.com")
 # 인증번호 입력 
+
 pw2 = iris_driver.find_element(By.CSS_SELECTOR, "#number1")
 pw2.click()
 pyperclip.copy(auth_num)
 pyautogui.hotkey("ctrl", "v")
-time.sleep(2)
+
+# #myInfoContainer > a
+# #myInfoViewList > li:nth-child(1) > a
+# #WorkStatusd 
+
+# #WorkStatusd 이거 출력해주면 됨. 
